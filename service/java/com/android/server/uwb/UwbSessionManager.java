5c66fa771142e00c619f376f7a20b45b8f58a06e
Roshan Pius <rpius@google.com>
2022-05-03 18:12:40 +0000

    uwb(service): Handle session deinit notification
    Cleanup ongoing session when session deinit notification is received.
    
    Bug: 230037507
    Test: atest ServiceUwbTests
    Change-Id: Iceede89bad8e95037ae94e87c1afbab4d28c57c7

--- service/java/com/android/server/uwb/UwbSessionManager.java
+++ service/java/com/android/server/uwb/UwbSessionManager.java
@@ -78,6 +78,7 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification 
     private static final int SESSION_STOP_RANGING = 3;
     private static final int SESSION_RECONFIG_RANGING = 4;
     private static final int SESSION_CLOSE = 5;
+    private static final int SESSION_ON_DEINIT = 6;
 
     // TODO: don't expose the internal field for testing.
     @VisibleForTesting
@@ -179,6 +180,9 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification 
                     //mSessionNotificationManager.onRangingReconfigureFailed(
                     //      uwbSession, reasonCode);
                 }
+                break;
+            case UwbUciConstants.UWB_SESSION_STATE_DEINIT:
+                mEventTask.execute(SESSION_ON_DEINIT, uwbSession);
                 break;
             default:
                 break;
@@ -383,14 +387,23 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification 
         Log.d(TAG, "deinitAllSession()");
         for (Map.Entry<Integer, UwbSession> sessionEntry : mSessionTable.entrySet()) {
             UwbSession uwbSession = sessionEntry.getValue();
-            mSessionNotificationManager.onRangingClosedWithApiReasonCode(uwbSession,
-                    RangingChangeReason.SYSTEM_POLICY);
-            mUwbMetrics.logRangingCloseEvent(uwbSession, UwbUciConstants.STATUS_CODE_OK);
-            removeSession(uwbSession);
+            onDeInit(uwbSession);
         }
 
         // Not resetting chip on UWB toggle off.
         // mNativeUwbManager.resetDevice(UwbUciConstants.UWBS_RESET);
+    }
+
+    public synchronized void onDeInit(UwbSession uwbSession) {
+        if (!isExistedSession(uwbSession.getSessionId())) {
+            Log.i(TAG, "onDeinit - Ignoring already deleted session " + uwbSession.getSessionId());
+            return;
+        }
+        Log.d(TAG, "onDeinit: " + uwbSession.getSessionId());
+        mSessionNotificationManager.onRangingClosedWithApiReasonCode(uwbSession,
+                RangingChangeReason.SYSTEM_POLICY);
+        mUwbMetrics.logRangingCloseEvent(uwbSession, UwbUciConstants.STATUS_CODE_OK);
+        removeSession(uwbSession);
     }
 
     public void setCurrentSessionState(int sessionId, int state) {
@@ -473,6 +486,12 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification 
                 case SESSION_CLOSE: {
                     UwbSession uwbSession = (UwbSession) msg.obj;
                     close(uwbSession);
+                    break;
+                }
+
+                case SESSION_ON_DEINIT : {
+                    UwbSession uwbSession = (UwbSession) msg.obj;
+                    onDeInit(uwbSession);
                     break;
                 }
 
