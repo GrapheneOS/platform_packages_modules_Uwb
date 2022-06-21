8db33b7894da71e64b1f44dad7af264bcb990fc8
Shreshta Manu <shreshtabm@google.com>
2022-06-21 16:18:52 +0000

    [uwb] Add state machine skeleton for pacs controlee and controller
    Bug: 197341285
    Test: atest ServiceUwbTests
    Change-Id: I6bae291fe6be04b49771dac7afea9582a80c1750

--- service/tests/src/com/android/server/uwb/profile/ProfileManagerTest.java
+++ service/tests/src/com/android/server/uwb/profile/ProfileManagerTest.java
@@ -18,11 +18,17 @@ package com.android.server.uwb.profile;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import android.content.AttributionSource;
 import android.content.Context;
 import android.os.Handler;
+import android.os.Looper;
 import android.platform.test.annotations.Presubmit;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.uwb.IUwbRangingCallbacks;
+import android.uwb.SessionHandle;
 
 import androidx.test.runner.AndroidJUnit4;
 
@@ -50,12 +56,18 @@ public class ProfileManagerTest {
     @Mock private Handler mHandler;
     @Mock private UwbConfigStore mUwbConfigStore;
     @Mock private UwbInjector mUwbInjector;
+    @Mock private SessionHandle mSessionHandle;
+    @Mock private AttributionSource mAttributionSource;
+    @Mock private IUwbRangingCallbacks mIUwbRangingCallbacks;
+    @Mock private Looper mLooper;
+
     private com.android.server.uwb.pm.ProfileManager mProfileManager;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
+        when(mHandler.getLooper()).thenReturn(mLooper);
         mProfileManager = new ProfileManager(mContext, mHandler, mUwbConfigStore, mUwbInjector);
 
     }
@@ -102,6 +114,36 @@ public class ProfileManagerTest {
         mProfileManager.loadServiceProfile(testMap);
 
         assertEquals(1, mProfileManager.mServiceProfileMap.size());
+    }
+
+    @Test
+    public void testActivateProfile() {
+        Optional<UUID> uuid1 = mProfileManager.addServiceProfile(1);
+        assertEquals(1, mProfileManager.mAppServiceProfileMap.size());
+
+        assertEquals(mProfileManager.mRangingSessionTable.size(), 0);
+
+        mProfileManager.activateProfile(mAttributionSource, mSessionHandle, uuid1.get(),
+                mIUwbRangingCallbacks);
+
+        assertEquals(mProfileManager.mRangingSessionTable.size(), 1);
+    }
+
+    @Test
+    public void testStartRanging() {
+        Optional<UUID> uuid1 = mProfileManager.addServiceProfile(1);
+        assertEquals(1, mProfileManager.mAppServiceProfileMap.size());
+
+        assertEquals(mProfileManager.mRangingSessionTable.size(), 0);
+
+        mProfileManager.activateProfile(mAttributionSource, mSessionHandle, uuid1.get(),
+                mIUwbRangingCallbacks);
+
+        mProfileManager.startRanging(mSessionHandle);
+
+        assertNotNull(mProfileManager.mRangingSessionTable.get(mSessionHandle));
+        assertEquals(mProfileManager.mRangingSessionTable.size(), 1);
+
     }
 
 }
