7cd42ec3064c5d9582f52cdb03cdbf432f3c765d
ziyiw <ziyiw@google.com>
2022-09-08 20:32:38 +0000

    Add supported channel to ranging capabilities.
    Test: manual
    Bug: 244747097
    Change-Id: Ia15a06c2d3d9993272b913c6043a78efe4a65d2a

--- androidx_backend/src/androidx/core/uwb/backend/impl/internal/RangingCapabilities.java
+++ androidx_backend/src/androidx/core/uwb/backend/impl/internal/RangingCapabilities.java
@@ -18,15 +18,21 @@ package androidx.core.uwb.backend.impl.internal;
 
 import androidx.annotation.IntRange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Describes UWB ranging capabilities for the current device. */
 public class RangingCapabilities {
     /** Default minimum ranging interval if the system API doesn't provide it. */
-    private static final int FIRA_DEFAULT_RANGING_INTERVAL_MS = 200;
+    public static final int FIRA_DEFAULT_RANGING_INTERVAL_MS = 200;
+    /** Default supported channel if the system API doesn't provide it. */
+    public static final int FIRA_DEFAULT_SUPPORTED_CHANNEL = 9;
 
     private final boolean mSupportsDistance;
     private final boolean mSupportsAzimuthalAngle;
     private final boolean mSupportsElevationAngle;
     private final int mMinRangingInterval;
+    private final List<Integer> mSupportedChannels;
 
     public RangingCapabilities(
             boolean supportsDistance,
@@ -36,18 +42,21 @@ public class RangingCapabilities {
                 supportsDistance,
                 supportsAzimuthalAngle,
                 supportsElevationAngle,
-                FIRA_DEFAULT_RANGING_INTERVAL_MS);
+                FIRA_DEFAULT_RANGING_INTERVAL_MS,
+                new ArrayList<Integer>(FIRA_DEFAULT_SUPPORTED_CHANNEL));
     }
 
     public RangingCapabilities(
             boolean supportsDistance,
             boolean supportsAzimuthalAngle,
             boolean supportsElevationAngle,
-            int minRangingInterval) {
+            int minRangingInterval,
+            List<Integer> supportedChannels) {
         this.mSupportsDistance = supportsDistance;
         this.mSupportsAzimuthalAngle = supportsAzimuthalAngle;
         this.mSupportsElevationAngle = supportsElevationAngle;
         this.mMinRangingInterval = minRangingInterval;
+        this.mSupportedChannels = supportedChannels;
     }
 
     /** Whether distance ranging is supported. */
@@ -73,5 +82,14 @@ public class RangingCapabilities {
     @IntRange(from = 0)
     public int getMinRangingInterval() {
         return mMinRangingInterval;
+    }
+
+    /**
+     * Gets the supported channel number.
+     *
+     * @hide
+     */
+    public List<Integer> getSupportedChannels() {
+        return mSupportedChannels;
     }
 }
