088299bf6ca1c4768a787a710e09833eeec02c67
ziyiw <ziyiw@google.com>
2023-05-25 18:25:24 +0000

    Add initationTime setter to CccStartRangingParams.
    Test: ServiceUwbTests
    Bug: 245854896
    Change-Id: I180677f34a219a215aa6936bdd4556bf7870d1bc

--- service/java/com/android/server/uwb/UwbShellCommand.java
+++ service/java/com/android/server/uwb/UwbShellCommand.java
@@ -705,6 +705,7 @@ public class UwbShellCommand extends BasicShellCommandHandler {
         CccStartRangingParams cccStartRangingParams = new CccStartRangingParams.Builder()
                 .setSessionId(cccOpenRangingParams.getSessionId())
                 .setRanMultiplier(cccOpenRangingParams.getRanMultiplier())
+                .setInitiationTimeMs(cccOpenRangingParams.getInitiationTimeMs())
                 .build();
         startRangingSession(
                 cccOpenRangingParams, cccStartRangingParams, cccOpenRangingParams.getSessionId(),
