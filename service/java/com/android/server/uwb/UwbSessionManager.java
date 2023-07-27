d71ef58ad17d4b9a94cc6baf4e0c27768a1fcbf2
Ayush Jain <ayushjain@google.com>
2023-07-27 16:58:23 +0000

    Notify ranging session closure after removing data.
    This CL changes the order between notifying the application about a
    ranging session closure and deleting it's data from the UWB
    SessionManager.
    
    Currently the UWB Session Manager first notifies the application about
    a ranging session closure, and then proceeds to remove the state
    associated with it. This can lead to race conditions, as the application
    may attempt to create a new ranging session (with same sessionID),
    before the SessionManager is able to clear out the state.
    
    Bug: 291291261
    Test: atest ServiceUwbTests FrameworkUwbTests CtsUwbTestCases CtsUwbMultiDeviceTestCase_FiraRangingTests
    Change-Id: Iba6446eacf03f13f2e373c59323e29f1a2663b48

--- service/java/com/android/server/uwb/UwbSessionManager.java
+++ service/java/com/android/server/uwb/UwbSessionManager.java
@@ -1708,13 +1708,9 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification,
                             status = mNativeUwbManager.deInitSession(uwbSession.getSessionId(),
                                     uwbSession.getChipId());
                             if (status != UwbUciConstants.STATUS_CODE_OK) {
-                                mSessionNotificationManager.onRangingClosed(uwbSession, status);
                                 return status;
                             }
                             uwbSession.getWaitObj().blockingWait();
-                            Log.i(TAG, "onRangingClosed - status : " + status);
-                            mSessionNotificationManager.onRangingClosed(uwbSession,
-                                    reason);
                         }
                         return status;
                     });
@@ -1725,7 +1721,6 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification,
                         IUwbAdapter.RANGING_SESSION_CLOSE_THRESHOLD_MS);
             } catch (TimeoutException e) {
                 Log.i(TAG, "Failed to Stop Ranging - status : TIMEOUT");
-                mSessionNotificationManager.onRangingClosed(uwbSession, status);
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
@@ -1734,6 +1729,12 @@ public class UwbSessionManager implements INativeUwbManager.SessionNotification,
             // Reset all UWB session timers when the session is de-initialized (ie, closed).
             uwbSession.stopTimers();
             removeSession(uwbSession);
+
+            // Notify about Session closure after removing it from the SessionTable.
+            Log.i(TAG, "onRangingClosed - status : " + status);
+            mSessionNotificationManager.onRangingClosed(uwbSession,
+                    status == STATUS_CODE_OK ? reason : status);
+
             Log.i(TAG, "deinit finish : status :" + status);
             Trace.endSection();
         }
