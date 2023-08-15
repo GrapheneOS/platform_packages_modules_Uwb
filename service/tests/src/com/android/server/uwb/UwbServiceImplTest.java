363651f68da73e9f03df1b101e5979a730e16ba0
Shreshta Manu <shreshtabm@google.com>
2023-08-15 21:27:47 +0000

    Revert "DO NOT MERGE: [uwb] Add flagging support to uwb"
    This reverts commit 76e65da77dbc3f34ce32d5bfcfca9f99803f7617.
    
    Reason for revert: This breaks hybrid manifest, re-land once aconfig flags are supported in mainline.
    
    Change-Id: Idc4ce6795a298ce41a16de27be3dc4743891a41a

--- service/tests/src/com/android/server/uwb/UwbServiceImplTest.java
+++ service/tests/src/com/android/server/uwb/UwbServiceImplTest.java
@@ -60,9 +60,6 @@ import android.os.Process;
 import android.os.UserManager;
 import android.os.test.TestLooper;
 import android.platform.test.annotations.Presubmit;
-import android.platform.test.annotations.RequiresFlagsEnabled;
-import android.platform.test.flag.junit.CheckFlagsRule;
-import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.provider.Settings;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.uwb.IOnUwbActivityEnergyInfoListener;
@@ -81,16 +78,15 @@ import com.android.server.uwb.data.UwbUciConstants;
 import com.android.server.uwb.jni.NativeUwbManager;
 import com.android.server.uwb.multchip.UwbMultichipData;
 import com.android.server.uwb.pm.ProfileManager;
-import com.android.uwb.flags.FeatureFlags;
-import com.android.uwb.flags.Flags;
 
+import com.google.uwb.support.fira.FiraParams;
 import com.google.uwb.support.fira.FiraRangingReconfigureParams;
+import com.google.uwb.support.fira.FiraSuspendRangingParams;
 import com.google.uwb.support.multichip.ChipInfoParams;
 import com.google.uwb.support.profile.ServiceProfile;
 import com.google.uwb.support.profile.UuidBundleWrapper;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
@@ -135,11 +131,6 @@ public class UwbServiceImplTest {
     private UwbServiceImpl mUwbServiceImpl;
     private TestLooper mTestLooper;
 
-    @Mock private FeatureFlags mFeatureFlags;
-    @Rule
-    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
-
-
     private void createUwbServiceImpl() {
         mUwbServiceImpl = new UwbServiceImpl(mContext, mUwbInjector);
     }
@@ -162,7 +153,6 @@ public class UwbServiceImplTest {
                 .thenReturn("cell,bluetooth,nfc,uwb,wifi");
         when(mUwbInjector.getNativeUwbManager()).thenReturn(mNativeUwbManager);
         when(mUwbInjector.getUserManager()).thenReturn(mUserManager);
-        when(mUwbInjector.getFeatureFlags()).thenReturn(mFeatureFlags);
         when(mUserManager.getUserRestrictions().getBoolean(anyString())).thenReturn(false);
         when(mUwbServiceCore.getHandler()).thenReturn(new Handler(mTestLooper.getLooper()));
 
@@ -814,13 +804,11 @@ public class UwbServiceImplTest {
     }
 
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_QUERY_TIMESTAMP_MICROS)
     public void testQueryDataSize() throws Exception {
-        assumeTrue(SdkLevel.isAtLeastV()); // Test should only run on V+ devices.
+        assumeTrue(SdkLevel.isAtLeastU()); // Test should only run on U+ devices.
         final SessionHandle sessionHandle = mock(SessionHandle.class);
         final PersistableBundle parameters = new PersistableBundle();
 
-        when(mFeatureFlags.queryTimestampMicros()).thenReturn(true);
         when(mUwbServiceCore.queryMaxDataSizeBytes(sessionHandle)).thenReturn(MAX_DATA_SIZE);
         assertThat(mUwbServiceImpl.queryMaxDataSizeBytes(sessionHandle)).isEqualTo(MAX_DATA_SIZE);
 
@@ -828,13 +816,11 @@ public class UwbServiceImplTest {
     }
 
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_HYBRID_SESSION_SUPPORT)
     public void testSetHybridSessionConfiguration() throws Exception {
         assumeTrue(SdkLevel.isAtLeastV()); // Test should only run on V+ devices.
         final SessionHandle sessionHandle = mock(SessionHandle.class);
         final PersistableBundle parameters = new PersistableBundle();
 
-        when(mFeatureFlags.hybridSessionSupport()).thenReturn(true);
         when(mUwbServiceCore.setHybridSessionConfiguration(sessionHandle, parameters))
                .thenReturn(TEST_STATUS);
         assertThat(mUwbServiceImpl.setHybridSessionConfiguration(sessionHandle, parameters))
