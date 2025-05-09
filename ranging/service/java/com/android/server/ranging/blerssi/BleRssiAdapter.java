06870481fdf8c51dabc313b807ce7bdc674b706e
Shreshta Manu <shreshtabm@google.com>
2025-05-09 17:27:47 +0000

    [Ranging] Add app attribution source for BLE  CS and RSSI ranging usage
    Bug: 414579353
    Test: atest RangingServiceTests, atest CtsRangingTestCases, manual test
    with ranging test app.
    Flag: EXEMPT: minor bug
    
    Change-Id: Ib6e586b0f9cbb069e0f88516507bf4d0bc16632a

--- ranging/service/java/com/android/server/ranging/blerssi/BleRssiAdapter.java
+++ ranging/service/java/com/android/server/ranging/blerssi/BleRssiAdapter.java
@@ -34,6 +34,7 @@ import android.bluetooth.le.DistanceMeasurementResult;
 import android.bluetooth.le.DistanceMeasurementSession;
 import android.content.AttributionSource;
 import android.content.Context;
+import android.content.ContextParams;
 import android.os.SystemClock;
 import android.ranging.DataNotificationConfig;
 import android.ranging.RangingData;
@@ -79,13 +80,17 @@ public class BleRssiAdapter implements RangingAdapter {
 
     private final AlarmManager.OnAlarmListener mMeasurementLimitListener;
 
-    public BleRssiAdapter(@NonNull Context context, RangingInjector rangingInjector) {
+    public BleRssiAdapter(@NonNull Context context,
+            AttributionSource attributionSource,
+            RangingInjector rangingInjector) {
         if (!RangingTechnology.RSSI.isSupported(context)) {
             throw new IllegalArgumentException("BT_RSSI system feature not found.");
         }
-        mContext = context;
+        mContext = context.createContext(
+                new ContextParams.Builder().setNextAttributionSource(attributionSource).build()
+        );
         mRangingInjector = rangingInjector;
-        mBluetoothAdapter = context.getSystemService(BluetoothManager.class).getAdapter();
+        mBluetoothAdapter = mContext.getSystemService(BluetoothManager.class).getAdapter();
         mStateMachine = new StateMachine<>(State.STOPPED);
         mCallbacks = null;
         mSession = null;
@@ -94,7 +99,7 @@ public class BleRssiAdapter implements RangingAdapter {
                 new DataNotificationConfig.Builder().build(),
                 new DataNotificationConfig.Builder().build()
         );
-        mAlarmManager = mContext.getSystemService(AlarmManager.class);
+        mAlarmManager = context.getSystemService(AlarmManager.class);
         mMeasurementLimitListener = () -> {
             Log.i(TAG, "Measurements limit exceeded. Stopping the session");
             Executors.newCachedThreadPool().execute(this::stop);
--- ranging/service/java/com/android/server/ranging/cs/CsAdapter.java
+++ ranging/service/java/com/android/server/ranging/cs/CsAdapter.java
@@ -36,6 +36,7 @@ import android.bluetooth.le.DistanceMeasurementResult;
 import android.bluetooth.le.DistanceMeasurementSession;
 import android.content.AttributionSource;
 import android.content.Context;
+import android.content.ContextParams;
 import android.os.CancellationSignal;
 import android.ranging.DataNotificationConfig;
 import android.ranging.RangingData;
@@ -89,12 +90,16 @@ public class CsAdapter implements RangingAdapter {
     private final AlarmManager.OnAlarmListener mMeasurementLimitListener;
 
     /** Injectable constructor for testing. */
-    public CsAdapter(@NonNull Context context, RangingInjector rangingInjector) {
+    public CsAdapter(@NonNull Context context,
+            AttributionSource attributionSource,
+            RangingInjector rangingInjector) {
         if (!RangingTechnology.CS.isSupported(context)) {
             throw new IllegalArgumentException("BT_CS system feature not found.");
         }
-        mContext = context;
-        mBluetoothAdapter = context.getSystemService(BluetoothManager.class).getAdapter();
+        mContext = context.createContext(
+                new ContextParams.Builder().setNextAttributionSource(attributionSource).build()
+        );
+        mBluetoothAdapter = mContext.getSystemService(BluetoothManager.class).getAdapter();
         mStateMachine = new StateMachine<>(State.STOPPED);
         mCallbacks = null;
         mSession = null;
@@ -103,7 +108,7 @@ public class CsAdapter implements RangingAdapter {
                 new DataNotificationConfig.Builder().build(),
                 new DataNotificationConfig.Builder().build()
         );
-        mAlarmManager = mContext.getSystemService(AlarmManager.class);
+        mAlarmManager = context.getSystemService(AlarmManager.class);
         mMeasurementLimitListener = () -> {
             Log.i(TAG, "Measurements limit exceeded. Stopping the session");
             Executors.newCachedThreadPool().execute(this::stop);
