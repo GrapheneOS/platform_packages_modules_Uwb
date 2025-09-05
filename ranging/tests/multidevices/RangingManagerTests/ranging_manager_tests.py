90cacdbee17219e46a7aa9562ec40c4fb2d0b5a3
James Eidson <jmes@google.com>
2025-09-05 19:03:35 +0000

    [test] Retry test_one_to_one_ble_cs_ranging on failure
    Bug: 441485008
    Test: atest MultiDeviceRangingTestCases:RangingManagerTest#test_one_to_one_ble_cs_ranging
    Flag: EXEMPT test-only change
    Change-Id: I089282a2e7f72f457a8d29ede9402b3e671d78ee

--- ranging/tests/multidevices/RangingManagerTests/ranging_manager_tests.py
+++ ranging/tests/multidevices/RangingManagerTests/ranging_manager_tests.py
@@ -30,6 +30,7 @@ from mobly import config_parser
 from mobly import signals
 from mobly import suite_runner
 from mobly.controllers import android_device
+from mobly.base_test import retry
 from android.platform.test.annotations import ApiTest
 
 
@@ -535,6 +536,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
           self.initiator.stop_ranging_and_assert_closed(SESSION_HANDLE)
           self._ble_disconnect()
 
+  @retry(max_count=2)
   def test_ble_cs_ranging_move_to_bg_and_fg(self):
       """ verifies ble cs ranging with foreground and background"""
       SESSION_HANDLE = str(uuid4())
@@ -1057,6 +1059,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
       'android.bluetooth.le.DistanceMeasurementSession#stopSession',
       'android.bluetooth.le.DistanceMeasurementParams#getMaxDurationSeconds',
   ])
+  @retry(max_count=2)
   def test_one_to_one_ble_cs_ranging(self):
     """
     Verifies cs ranging with peer device, devices range for 10 seconds.
@@ -1152,6 +1155,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
     session.assert_received_data()
     session.stop_and_assert_closed()
 
+  @retry(max_count=2)
   def test_one_to_one_ble_cs_ranging_with_oob(self):
     asserts.skip_if(self.initiator.ad.adb.getprop("ro.build.type") == "user",
                     "Skipping OOB CS test on user build because BLE address is masked")
@@ -1203,6 +1207,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
       session.stop_and_assert_closed(check_responders=False)
       self._ble_unbond()
 
+  @retry(max_count=2)
   def test_ble_cs_ranging_measurement_limit(self):
       """Verifies ble cs ranging with measurement limit."""
       asserts.skip_if(self._is_emulator_device(self.initiator.ad),
