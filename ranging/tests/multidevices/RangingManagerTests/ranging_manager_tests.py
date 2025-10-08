543c3191db3659c20563479657e71f766878a665
Kolin Lu <kolinlu@google.com>
2025-10-07 17:56:16 -0700

    Refactor: Improve test teardown and rename the app switch tests.
    The `teardown_test` now clears ranging sessions and creates output excerpts concurrently for all devices. Test names related to the app switch have also been updated to avoid confusion with the real moving tests.
    
    Flag: TEST_ONLY
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:d1fff9ea89cec48920840c60a58334bb74e97969
    Merged-In: I2533369d4edb5fb5b84ac3f1eb0fadc18244c187
    Change-Id: I2533369d4edb5fb5b84ac3f1eb0fadc18244c187

--- ranging/tests/multidevices/RangingManagerTests/ranging_manager_tests.py
+++ ranging/tests/multidevices/RangingManagerTests/ranging_manager_tests.py
@@ -27,8 +27,8 @@ from lib.params import *
 from lib.ranging_decorator import *
 from mobly import asserts
 from mobly import config_parser
-from mobly import signals
 from mobly import suite_runner
+from mobly import utils as mobly_utils
 from mobly.controllers import android_device
 from mobly.base_test import retry
 from android.platform.test.annotations import ApiTest
@@ -52,9 +52,9 @@ _TEST_CASES = [
     "test_one_to_one_ble_rssi_ranging_with_oob",
     "test_oob_responder_persists_until_explicitly_stopped",
     "test_dynamic_peer_uwb_ranging",
-    "test_uwb_ranging_move_to_bg_and_fg",
-    "test_ble_rssi_ranging_move_to_bg_and_fg",
-    "test_ble_cs_ranging_move_to_bg_and_fg",
+    "test_uwb_ranging_app_switch_to_bg_and_fg",
+    "test_ble_rssi_ranging_app_switch_to_bg_and_fg",
+    "test_ble_cs_ranging_app_switch_to_bg_and_fg",
 ]
 
 
@@ -121,8 +121,16 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
 
   def teardown_test(self):
     super().teardown_test()
-    for device in self.devices:
-      device.clear_ranging_sessions()
+    mobly_utils.concurrent_exec(
+        lambda d: d.clear_ranging_sessions(),
+        param_list=[[device] for device in self.devices],
+        raise_on_exception=True,
+    )
+    mobly_utils.concurrent_exec(
+        lambda d: d.services.create_output_excerpts_all(self.current_test_info),
+        param_list=[[device] for device in self.devices],
+        raise_on_exception=True,
+    )
 
   ### Helpers ###
 
@@ -395,7 +403,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
     self.initiator.stop_ranging_and_assert_closed(SESSION_HANDLE)
     self.responder.stop_ranging_and_assert_closed(SESSION_HANDLE)
 
-  def test_uwb_ranging_move_to_bg_and_fg(self):
+  def test_uwb_ranging_app_switch_to_bg_and_fg(self):
       """ verifies Uwb ranging with foreground and background"""
       SESSION_HANDLE = str(uuid4())
       UWB_SESSION_ID = 5
@@ -473,7 +481,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
       self.initiator.stop_ranging_and_assert_closed(SESSION_HANDLE)
       self.responder.stop_ranging_and_assert_closed(SESSION_HANDLE)
 
-  def test_ble_rssi_ranging_move_to_bg_and_fg(self):
+  def test_ble_rssi_ranging_app_switch_to_bg_and_fg(self):
       """ verifies ble rssi ranging with foreground and background"""
       SESSION_HANDLE = str(uuid4())
       TECHNOLOGIES = {RangingTechnology.BLE_RSSI}
@@ -537,7 +545,7 @@ class RangingManagerTest(ranging_base_test.RangingBaseTest):
           self._ble_disconnect()
 
   @retry(max_count=2)
-  def test_ble_cs_ranging_move_to_bg_and_fg(self):
+  def test_ble_cs_ranging_app_switch_to_bg_and_fg(self):
       """ verifies ble cs ranging with foreground and background"""
       SESSION_HANDLE = str(uuid4())
       TECHNOLOGIES = {RangingTechnology.BLE_CS}
