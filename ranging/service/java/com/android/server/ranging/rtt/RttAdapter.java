0830491272ed8530ca8d02bd092e6b37a21e72de
Shreshta Manu <shreshtabm@google.com>
2026-03-12 16:15:28 -0700

    Replace closeForReason with mCallbacks.onClosed in adapter start failures.
    When an adapter fails to start due to various reasons (e.g., background ranging policy, Bluetooth off, internal error, invalid config), directly call mCallbacks.onClosed with the appropriate InternalReason. This streamlines the error handling during the start process.
    
    Bug: 492203661
    Test: compiles
    Flag: EXEMPT BUGFIX
    Change-Id: I39b4ad47a0629895d909ddfcba953cef5fe80ddc

--- ranging/service/java/com/android/server/ranging/rtt/RttAdapter.java
+++ ranging/service/java/com/android/server/ranging/rtt/RttAdapter.java
@@ -175,7 +175,7 @@ public class RttAdapter implements RangingAdapter {
                 mNonPrivilegedAttributionSource.getUid(),
                 mNonPrivilegedAttributionSource.getPackageName())) {
             Log.w(TAG, "Background ranging is not supported");
-            closeForReason(InternalReason.BACKGROUND_RANGING_POLICY);
+            mCallbacks.onClosed(InternalReason.BACKGROUND_RANGING_POLICY);
             return;
         }
         if (!(config instanceof RttConfig rttConfig)) {
@@ -185,7 +185,7 @@ public class RttAdapter implements RangingAdapter {
         }
         if (!mStateMachine.transition(State.STOPPED, State.STARTED)) {
             Log.v(TAG, "Attempted to start adapter when it was already started");
-            closeForReason(InternalReason.INTERNAL_ERROR);
+            mCallbacks.onClosed(InternalReason.INTERNAL_ERROR);
             return;
         }
         mConfig = rttConfig;
